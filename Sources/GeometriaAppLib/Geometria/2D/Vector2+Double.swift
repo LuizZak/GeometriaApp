d31a4bd22ba3078269b3cefe5b99831a4d388c77
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2021-09-08 14:26:02 -0300

    Minor changes to imported Geometria code

--- Sources/GeometriaAppLib/Geometria/2D/Vector2+Double.swift
+++ Sources/GeometriaAppLib/Geometria/2D/Vector2+Double.swift
@@ -15,10 +15,7 @@ public extension Vector2D {
                            rotate angle: Scalar = 0,
                            translate: Self = .zero) -> double3x3 {
         
-        typealias MatrixType = double3x3
-        typealias Vector3Type = SIMD3<Scalar>
-        
-        var matrix = MatrixType(1)
+        var matrix = double3x3(1)
         
         // Prepare matrices
         
@@ -30,10 +27,11 @@ public extension Vector2D {
         //
         
         let cScale =
-            MatrixType(columns:
-                (Vector3Type(scale.x, 0, 0),
-                 Vector3Type(0, scale.y, 0),
-                 Vector3Type(0, 0, 1)))
+        double3x3(columns: (
+            .init(scale.x, 0, 0),
+            .init(0, scale.y, 0),
+            .init(0, 0, 1))
+        )
         
         matrix *= cScale
         
@@ -48,10 +46,11 @@ public extension Vector2D {
             let s = sin(-angle)
             
             let cRotation =
-                MatrixType(columns:
-                    (Vector3Type(c, s, 0),
-                     Vector3Type(-s, c, 0),
-                     Vector3Type(0, 0, 1)))
+            double3x3(columns: (
+                .init(c, s, 0),
+                .init(-s, c, 0),
+                .init(0, 0, 1))
+            )
             
             matrix *= cRotation
         }
@@ -64,10 +63,11 @@ public extension Vector2D {
         //
         
         let cTranslation =
-            MatrixType(columns:
-                (Vector3Type(1, 0, translate.x),
-                 Vector3Type(0, 1, translate.y),
-                 Vector3Type(0, 0, 1)))
+        double3x3(columns: (
+            .init(1, 0, translate.x),
+            .init(0, 1, translate.y),
+            .init(0, 0, 1))
+        )
         
         matrix *= cTranslation
         
