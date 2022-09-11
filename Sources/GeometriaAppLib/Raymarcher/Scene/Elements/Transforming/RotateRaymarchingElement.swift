2e923f6589a00db37b4a8ee3b833dc8db5abf44e
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-09-11 19:17:17 -0300

    Fixing rotate raymarching element

--- Sources/GeometriaAppLib/Raymarcher/Scene/Elements/Transforming/RotateRaymarchingElement.swift
+++ Sources/GeometriaAppLib/Raymarcher/Scene/Elements/Transforming/RotateRaymarchingElement.swift
@@ -3,10 +3,12 @@ typealias RotateRaymarchingElement<T: RaymarchingElement> = RotateElement<T>
 extension RotateRaymarchingElement: RaymarchingElement {
     @inlinable
     func signedDistance(to point: RVector3D, current: RaymarchingResult) -> RaymarchingResult {
-        return element.signedDistance(
+        let result = element.signedDistance(
             to: point.rotated(by: rotationInv, around: rotationCenter),
             current: .emptyResult()
         )
+
+        return min(current, result)
     }
 
     func accept<Visitor: RaymarchingElementVisitor>(_ visitor: Visitor) -> Visitor.ResultType {
