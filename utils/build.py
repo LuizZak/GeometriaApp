5fcbbcd38e8712183a19409450e7a76b4ae7b914
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2024-06-27 18:12:09 -0300

    Fixing build

--- utils/build.py
+++ utils/build.py
@@ -23,7 +23,6 @@ win32_debug_args = [
     "-Xlinker", "/ignore:4217", # For ignoring a very verbose set of warnings in debug builds on Windows
 ]
 win32_release_args = [
-    "-Xswiftc", "-whole-module-optimization",
     "-Xlinker", "/ignore:4217", # For ignoring a very verbose set of warnings in debug builds on Windows
 ]
 
